1aed4f1d7cf59579f47c53f6f8389fe3a2b07e8e
SUSSEXDOWNS\465277 <razor78790@gmail.com>
2018-01-16 14:21:42 +0000

    This is just more coding for parallaxing

--- 2D runner map design/Assets/MyAssets/MyScripts/Parallaxing.cs
+++ 2D runner map design/Assets/MyAssets/MyScripts/Parallaxing.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Parallaxing : MonoBehaviour {
 
+	public Transform[] backgrounds;				//This is and array of all the background and foregrounds to be parallaxed
+	private float[] 
+
+
 	// Use this for initialization
 	void Start () {
 		
